3371267c5fee5e46f03e15e37976fc512bc0e59d
Admin <Admin@DESKTOP-7IOJSK2>
2024-11-20 13:54:35 +0530

    pollscm class added

---
+++ AutomationFrameWork/src/test/java/pollscm/pollScmTest.java
@@ -0,0 +1,12 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class pollScmTest {
+	@Test
+	public void scm() {
+		Reporter.log("poll scm test executed",true);
+	}
+
+}
